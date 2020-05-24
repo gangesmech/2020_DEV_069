cdb38b3f939a2d57447122f9e1327364c5486bb5
rajakanganathan <rajakanganathan85@gmail.com>
2020-05-24 14:57:48 +0530

    Implementation - Board updates successfully for valid position

--- src/main/java/com/kata/game/tictactoe/Board.java
+++ src/main/java/com/kata/game/tictactoe/Board.java
@@ -1,8 +1,30 @@
 package com.kata.game.tictactoe;
 
-public class Board {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
 
+public class Board {
+	
+	private List<Cell> cells;
+	private static final int BOARD_SIZE = 3;
+	
+	public void initialize() {
+		this.cells = new ArrayList<>(BOARD_SIZE * BOARD_SIZE);		
+		IntStream.rangeClosed(1, BOARD_SIZE).forEach(
+				row -> IntStream.rangeClosed(1, BOARD_SIZE).forEach(column -> cells.add(new Cell(row, column))));
+	}
+	
 	public boolean update(int rowSelected, int columnSelected, Mark mark) {
+		if(cells == null || cells.isEmpty()) {
+			this.initialize();
+		}
+		Cell cell = cells.stream().filter(e -> e.getColumn() == columnSelected && e.getRow() == rowSelected).findFirst().orElse(null);
+		if (cell != null) {
+			cell.setMark(mark);
+			return true;
+		}
 		return false;
 	}
+	
 }
---
+++ src/main/java/com/kata/game/tictactoe/Cell.java
@@ -0,0 +1,31 @@
+package com.kata.game.tictactoe;
+
+public class Cell{
+	
+	private Mark mark;
+	
+	private int row;
+	
+	private int column;
+		
+	public Cell(int row, int column) {
+		this.row = row;
+		this.column = column;
+	}
+	
+	public Mark getMark() {
+		return mark;
+	}
+	
+	public void setMark(Mark mark) {
+		this.mark = mark;
+	}
+	
+	public int getColumn() {
+		return column;
+	}
+	
+	public int getRow() {
+		return row;
+	}	
+}
