9da6f3524ceddb489884e8393788998beece5647
rajakanganathan <rajakanganathan85@gmail.com>
2020-05-24 23:43:43 +0530

    Implementation - Game runner(Play) : Command line user input

--- src/main/java/com/kata/game/tictactoe/Play.java
+++ src/main/java/com/kata/game/tictactoe/Play.java
@@ -1,12 +1,15 @@
 package com.kata.game.tictactoe;
 
 import static java.util.logging.Level.INFO;
+
+import java.util.Scanner;
 import java.util.logging.Logger;
 
 public class Play {	
 	
 	private Board board;
-	private static final int BOARD_SIZE = 3;
+	private static final int BOARD_SIZE = 3;	
+	private static Scanner scanner = new Scanner(System.in);
 	private static final Logger LOGGER = Logger.getLogger(Play.class.getName());
 
 	void initialize() {
@@ -36,6 +39,37 @@ public class Play {
 			return true;
 		}
 		return false;
+	}
+	
+	void display() {
+		StringBuilder stringBuilder = new StringBuilder("\n");
+		board.getCells().stream().forEach(cell -> {
+			stringBuilder.append(cell.getMark().getLabel());
+			if (BOARD_SIZE == cell.getColumn()) {
+				stringBuilder.append("\n");
+			} else {
+				stringBuilder.append(" | ");
+			}
+		});
+		LOGGER.log(INFO, "{0}", stringBuilder);
+	}
+	
+	
+	void userInput(Mark player) {
+		do {
+			 LOGGER.log(INFO, "Player {0}, please choose the position [row(1-{1}) column(1-{1})]: ", new Object[]{player.getLabel(), BOARD_SIZE});	             
+	    } while(!updateBoard(scanner.nextInt(), scanner.nextInt(), player));
+	}
+		
+	public static void main(String[] args) {
+		 Play play = new Play();
+		 play.initialize();
+		 Mark player = Mark.CROSS;
+		 do {
+			 play.userInput(player);
+			 player = (Mark.CROSS == play.getBoard().getCellSelected().getMark()) ? Mark.NOUGHT : Mark.CROSS;
+			 play.display();
+		} while(!play.hasResult());	
 	}
 	
 	public Board getBoard() {
